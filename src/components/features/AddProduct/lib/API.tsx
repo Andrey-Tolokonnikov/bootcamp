503d8c14b2e4ee165c81365ba497a8234d13131a
Andrey-Tolokonnikov <vorbedeutunug@gmail.com>
2024-08-26 20:10:26 +0300

    category_id to int

--- src/components/features/AddProduct/lib/API.tsx
+++ src/components/features/AddProduct/lib/API.tsx
@@ -1,10 +1,17 @@
 import { ProductType } from "@/components/entities/Product/model/Product"
 import { toast } from "@/components/ui/use-toast"
 
+
+const getProductWithCategoryInt = (product: ProductType) => {
+  return {
+    ...product,
+    category_id: parseInt(product.category_id as string)
+  }
+}
 function addProduct(product: ProductType){
   return fetch(`${import.meta.env.VITE_API_URL}/products/`, {
     method: "POST",
-    body: JSON.stringify(product),
+    body: JSON.stringify(getProductWithCategoryInt(product)),
     headers: {
       "Content-Type": "application/json"
     }
